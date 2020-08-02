ff7aee402eb0c9275945873fda61ca2290458dec
viharika123 <viharikabhavanasi@gmail.com>
2020-08-02 07:18:47 -0500

    deploy

---
+++ src/components/Lin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Style2.css'
+const Lin = () => {
+    return(
+    <div>  
+      <h1 className ="head"> Health is wealth </h1>
+      <div className="para"> Paragraph </div>
+      <a href = "/He" className="back"> Back to home </a>
+    </div>
+    )
+}
+export default Lin
+
